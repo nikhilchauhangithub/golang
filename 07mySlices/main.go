287039562760b7c43d690394c567fbf5e585531f
Nikhil <thisisnikhilchauhan@gmail.com>
2023-03-30 23:17:39 +0530

    sorting,append in slice

--- 07mySlices/main.go
+++ 07mySlices/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	fmt.Println("About slices")
@@ -15,6 +18,20 @@ func main() {
 	fruitsList = append(fruitsList[2:4]) //remove 2 fruits from starting and 4 means it will stop counting at element n-1 which means 4-1=3, at 3rd element
 	fmt.Println(fruitsList)
 
+highScores := make([]int, 4)
+highScores[0] =123
+highScores[1] =5152
+highScores[2] =12515
+highScores[3] =12545
 
+highScores = append(highScores, 0,11,22,33) //append reallocate all memories again
+
+fmt.Println(highScores)
+
+
+sort.Ints(highScores)
+
+fmt.Println(highScores)
+fmt.Println(sort.IntsAreSorted(highScores))
 
 }
