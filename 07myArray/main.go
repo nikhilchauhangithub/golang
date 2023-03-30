b38f4e0c0f8f40656d75fbdfd760f8a42bf62ee6
Nikhil <thisisnikhilchauhan@gmail.com>
2023-03-30 21:55:39 +0530

    array

---
+++ 07myArray/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("array in golnag")
+
+	var fruitsList [4]string
+
+	fruitsList [0] ="Apple"
+	fruitsList [1] ="Guava"
+	fruitsList [2] ="Peach"
+
+	fmt.Println("Here is the fruit: ", fruitsList[1])
+
+}
