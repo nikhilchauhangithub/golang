6c6145a736a64cd24ded92fef3613164800e22f4
Nikhil <thisisnikhilchauhan@gmail.com>
2023-03-30 22:04:33 +0530

    another array syntax

--- 07myArray/main.go
+++ 07myArray/main.go
@@ -8,9 +8,15 @@ func main() {
 	var fruitsList [4]string
 
 	fruitsList [0] ="Apple"
-	fruitsList [1] ="Guava"
+	fruitsList [1] ="Guavaa"
 	fruitsList [2] ="Peach"
 
 	fmt.Println("Here is the fruit: ", fruitsList[1])
+	fmt.Println("Length of array: ", len(fruitsList[1])) //this gives the length of word guava
+
+	//another way of writing an array
+
+	var vegetable = [3] string {"potato", "tomato", "beans"}
+	fmt.Println("Here is the veggies: ", vegetable)
 
 }
