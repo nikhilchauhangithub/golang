62e331d4d2bacb797d09b7608ba8b58830455ecd
Nikhil <thisisnikhilchauhan@gmail.com>
2023-04-05 14:47:49 +0530

    Get one course based on request id

--- 22buildApi/main.go
+++ 22buildApi/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
@@ -43,4 +45,21 @@ func getAllCourses(w http.ResponseWriter, r *http.Request)  {
 	fmt.Println("get all courses")
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(courses) // json.NewEncoder(w) creates a new json.Encoder instance that writes JSON data to the http.ResponseWriter instance w. By using an Encoder to encode the JSON data, the function can stream the JSON data directly to the response writer without buffering it in memory. This is more memory-efficient than buffering the JSON data in memory first and then writing it to the response writer, especially if the JSON data is large.
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("content-Type", "application/json")
+
+	//grab id from request
+	params :=mux.Vars(r) //it is used extract course id from user's requested url and send it to params
+
+	//loop through courses, find matching id and return the response to user
+
+	for _,course:=range courses{
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(fmt.Sprintf("No such course found with given id:%v\n",params["id"]))
 }
